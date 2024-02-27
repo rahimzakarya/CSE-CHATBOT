c6eca79849b130fc2af14d51cd7c2527a4359b3f
Zakarya RAHIM <rahimzakarya213@gmail.com>
2024-02-27 22:38:17 +0100

    EDITS

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ from flask import Flask, request, jsonify
 from Chat import get_response
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
-
 @app.route("/chat", methods=["POST"])
 def chat():
     text = request.get_json().get("message")
@@ -15,5 +11,5 @@ def chat():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run()
 # waitress-serve --listen=127.0.0.1:8000 app:app
